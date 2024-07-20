44ac66daa3b41ca79bd5749b06cfb216e1d8bcc0
orlando1080 <lewislsewell@gmail.com>
2024-07-20 16:07:28 +0100

    Complete The hero editor

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IHero } from '../ihero.interface';
 
 @Component({
   selector: 'app-heroes',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './heroes.component.css'
 })
 export class HeroesComponent {
+  hero: IHero = {
+    id: 1,
+    name: 'Windstrom'
+  };
 
 }
